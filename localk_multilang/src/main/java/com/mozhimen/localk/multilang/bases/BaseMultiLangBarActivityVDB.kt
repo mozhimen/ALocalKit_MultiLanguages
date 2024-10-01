639af6176cc1174a6b1c53232fffe6a5ba31e973
mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-10-01 11:53:14 +0800

    1001

--- localk_multilang/src/main/java/com/mozhimen/localk/multilang/bases/BaseMultiLangBarActivityVDB.kt
+++ localk_multilang/src/main/java/com/mozhimen/localk/multilang/bases/BaseMultiLangBarActivityVDB.kt
@@ -3,7 +3,7 @@ package com.mozhimen.localk.multilang.bases
 import android.content.Context
 import androidx.databinding.ViewDataBinding
 import com.hjq.language.MultiLanguages
-import com.mozhimen.mvvmk.bases.activity.databinding.BaseBarActivityVDB
+import com.mozhimen.bindk.bases.activity.databinding.BaseBarActivityVDB
 
 /**
  * @ClassName BaseMultiLangBarActivity
